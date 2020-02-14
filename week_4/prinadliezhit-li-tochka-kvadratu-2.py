25f8729b863c2fa20e61364b36d78925d57461b0
Ildar Baydamshin <ildar3s@mail.ru>
2020-02-15 00:28:33 +0300

    Solved task

--- week_4/prinadliezhit-li-tochka-kvadratu-2.py
+++ week_4/prinadliezhit-li-tochka-kvadratu-2.py
@@ -4,12 +4,29 @@
     Если точка принадлежит квадрату, выведите слово YES,
     иначе выведите слово NO. На рисунке сетка проведена с шагом 1.
 
-    https://www.coursera.org/learn/python-osnovy-programmirovaniya/programming/
-    oxiW1/prinadliezhit-li-tochka-kvadratu-2
+    https://www.coursera.org/learn/python-osnovy-programmirovaniya/
+    programming/oxiW1/prinadliezhit-li-tochka-kvadratu-2
 
-    Решение должно содержать функцию IsPointInSquare(x, y), возвращающую True, если точка принадлежит квадрату и False, если не принадлежит. Основная программа должна считать координаты точки, вызвать функцию IsPointInSquareи в зависимости от возвращенного значения вывести на экран необходимое сообщение.
+    Решение должно содержать функцию IsPointInSquare(x, y),
+    возвращающую True, если точка принадлежит квадрату и False,
+    если не принадлежит. Основная программа должна считать координаты точки,
+    вызвать функцию IsPointInSquareи в зависимости от возвращенного значения
+    вывести на экран необходимое сообщение.
 
     Функция IsPointInSquare не должна содержать инструкцию if.
 
 
 """
+
+
+def is_point_in_square(x, y):
+    return abs(x) + abs(y) <= 1
+
+
+x = float(input())
+y = float(input())
+
+if is_point_in_square(x, y):
+    print('YES')
+else:
+    print('NO')
