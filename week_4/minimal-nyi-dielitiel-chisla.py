67c9276f54a3cf451d243f32644af569d6b9e58f
Ildar Baydamshin <ildar3s@mail.ru>
2020-02-18 08:23:53 +0500

    Solved task

--- week_4/minimal-nyi-dielitiel-chisla.py
+++ week_4/minimal-nyi-dielitiel-chisla.py
@@ -1,10 +1,25 @@
+"""
+    Дано натуральное число n>1. Выведите его наименьший делитель,
+    отличный от 1. Решение оформите в виде функции MinDivisor(n).
+    Алгоритм должен иметь сложность порядка корня квадратного из n.
 
-def min_devisor(x):
-    d = 2
-    while x % d:
-        d += 1
-    return d
+    Указание. Если у числа n нет делителя не превосходящего корня из n,
+    то число n — простое и ответом будет само число n.
+    А у всех составных чисел обязательно есть делители,
+    отличные от единицы и не превосходящие корня из n.
+
+"""
 
 
-print(min_devisor(int(input())))
+def min_divisor(n):
+    import math
+    divisor = 2
+    while n % divisor:
+        if divisor >= math.sqrt(n):
+            return n
+        divisor += 1
+    return divisor
 
+
+result = min_divisor(int(input()))
+print(result)
