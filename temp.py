e9913238359e192f0b2a48267066713b7b388656
Ildar Baydamshin <ildar3s@mail.ru>
2020-02-21 20:22:46 +0500

    Solved task

--- temp.py
+++ temp.py
@@ -5,5 +5,14 @@ def rec():
         print(n)
 
 
-# rec()
+def f(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return f(n - 2) + f(n - 1)
 
+
+print(f(8))
+
+# rec()
