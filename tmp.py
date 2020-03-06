eef95110c4cee45f81fe506cf8bc5ff124783b99
Ildar Baydamshin <ildar3s@mail.ru>
2020-03-06 08:19:45 +0500

    Solved

--- tmp.py
+++ tmp.py
@@ -1,11 +1,32 @@
-def f(n):
-    if n == 0:
-        return n
-    i = 0
-    m = int((n ** .5) - i)
-    print(m ** 2, end=' ')
-    f(n - m ** 2)
+def lg4(n):
+    d = 0
+    d_max = int(n ** .5)
+    while d <= d_max:
+        c = 0
+        c_max = int(n ** .5)
+        while c <= c_max:
+            b = 0
+            b_max = int(n ** .5)
+            while b <= b_max:
+                a = 0
+                a_max = int(n ** .5)
+                while a <= a_max:
+                    if n == a ** 2 + b ** 2 + c ** 2 + d ** 2:
+                        # print(a ** 2, b ** 2, c ** 2, d ** 2)
+                        return a, b, c, d
+                    a += 1
+                b += 1
+            c += 1
+        d += 1
 
 
-n = 23
-f(n)
+# print(lg4(23))
+
+
+t = 0
+for i in range(101):
+    if lg4(i)[0]:
+        print('_' * 10)
+        print(i, lg4(i))
+        t += 1
+print('\n', 'OK', t)
