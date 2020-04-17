608c5118a7a95a9f341deb1d2f3d78943d79d7a6
Ildar Baydamshin <ildar3s@mail.ru>
2020-04-17 15:57:53 +0300

    Solved

---
+++ week_7/kolichiestvo-slov-v-tiekstie.py
@@ -0,0 +1,30 @@
+"""
+    Во входном файле (вы можете читать данные из sys.stdin, подключив
+    библиотеку sys) записан текст. Словом считается последовательность
+    непробельных символов идущих подряд, слова разделены одним или большим
+    числом пробелов или символами конца строки. Определите, сколько различных
+    слов содержится в этом тексте.
+
+    Формат ввода
+    Вводится текст.
+
+    Формат вывода
+    Выведите ответ на задачу.
+
+Примеры
+Тест 1
+Входные данные:
+She sells sea shells on the sea shore;
+The shells that she sells are sea shells I'm sure.
+So if she sells sea shells on the sea shore,
+I'm sure that the shells are sea shore shells.
+
+
+Вывод программы:
+19
+
+"""
+
+import sys
+
+print(len(set(sys.stdin.read().split())))
