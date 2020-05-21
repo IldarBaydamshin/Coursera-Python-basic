57d316e37dcb61d8f02963c2e6460098bb74e128
Ildar Baydamshin <ildar3s@mail.ru>
2020-05-21 18:28:13 +0300

    In procces

--- week_8/stavki_v2.py
+++ week_8/stavki_v2.py
@@ -16,14 +16,8 @@ f1 = map(lambda kn: int(kn), fin.readline().split())
 f2 = tuple(zip(*((lambda x, y: (
     tuple(itertools.permutations(range(1, x + 1))),
     tuple(itertools.repeat(
-        tuple(itertools.chain(
-            *map(lambda bet:
-                 (bet[:2], bet[2:]),
-                 map(lambda _:
-                     tuple(map(int, fin.readline().split())),
-                     range(y))))),
-        len(tuple(itertools.permutations(range(x))))))))(*f1))))
+            tuple(map(lambda i: (i[:2], i[2:]), map(lambda _: tuple(map(int, fin.readline().split())), range(y)))),
+        len(tuple(itertools.permutations(range(x))))
+    ))))(*f1))))
 
-pprint(tuple(map(lambda a:
-                 (a[0], a[1])
-                 , f2)))
+pprint(f2)
