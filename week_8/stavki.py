086ae7890842ba0fa7ac3c771c35b57b2663c4b5
Ildar Baydamshin <ildar3s@mail.ru>
2020-05-28 13:12:54 +0300

    Solved

--- week_8/stavki.py
+++ week_8/stavki.py
@@ -8,10 +8,9 @@ print(next(map(lambda a:
                                         a[1])), (
                           lambda a, b: zip(itertools.permutations(a),
                                            itertools.repeat(b)))(
-                   *(lambda k, n: (
-                       range(1, k + 1),
-                       tuple(map(lambda i: (i[:2], i[2:]),
-                                 map(lambda _: tuple(
-                                     map(int, input().split())),
-                                     range(n)))))
+                   *(lambda k, n: (range(1, k + 1),
+                                   tuple(map(lambda i: (i[:2], i[2:]),
+                                             map(lambda _: tuple(
+                                                 map(int, input().split())),
+                                                 range(n)))))
                      )(*map(int, input().split()))))), 0))
