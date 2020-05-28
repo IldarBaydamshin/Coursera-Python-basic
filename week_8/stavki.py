f1c2ac173da429d18ef02e7461d46fc3ffe4f876
Ildar Baydamshin <ildar3s@mail.ru>
2020-05-28 13:10:55 +0300

    In procces

--- week_8/stavki.py
+++ week_8/stavki.py
@@ -1,20 +1,17 @@
 import itertools
 import operator
 
-print(next(map(lambda res: res[0], filter(lambda res: res[1], map(lambda a: (
-    ' '.join(map(str, a[0])),
-    all(map(lambda b:
-                  operator.xor(*map(lambda c:
-                                    c in itertools.combinations(a[0], 2), b)),
-                  a[1]))),
-         map(lambda a: a, (lambda a, b:
-                                 (dict.fromkeys(
-                                     itertools.permutations(range(1, a + 1)),
-                                     tuple(map(lambda i:
-                                               (i[:2], i[2:]),
-                                               map(lambda _:
-                                                   tuple(map(int,
-                                                             input().split())),
-                                                   range(b))))))
-                                 )(
-             *map(lambda kn: int(kn), input().split())).items())))), 0))
+print(next(map(lambda a:
+               ' '.join(map(str, a[0])),
+               filter(lambda a: all(map(lambda b: operator.xor(
+                   *map(lambda c: c in itertools.combinations(a[0], 2), b, )),
+                                        a[1])), (
+                          lambda a, b: zip(itertools.permutations(a),
+                                           itertools.repeat(b)))(
+                   *(lambda k, n: (
+                       range(1, k + 1),
+                       tuple(map(lambda i: (i[:2], i[2:]),
+                                 map(lambda _: tuple(
+                                     map(int, input().split())),
+                                     range(n)))))
+                     )(*map(int, input().split()))))), 0))
